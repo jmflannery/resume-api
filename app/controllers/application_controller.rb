6250b397ae5cfab8c2bb7ed6b2750c299877f86e
Jack Flannery <jmflannery81@gmail.com>
2013-12-13 10:15:11 -0500

    add authentication checks in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def toke
+    head :unauthorized unless current_user
+  end
+
+  def current_user
+    token = Toke::Token.active.where(key: toke_key).first
+    token.user if token
+  end
+
+  def toke_key
+    request.headers['X-Toke-Key']
+  end
 end
